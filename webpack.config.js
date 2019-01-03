1dbe825866b3c449e4fe7648a84aa47ba1e10bc9
Unknown <adantoscano@gmail.com>
2019-01-03 21:30:15 +0100

    document webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = {
+  /*
+    Entry point, file where you app renders to your DOM(ReactDOM.render)
+    add more [key]: [value] as many entry points you have,
+    key= name of your app
+    value= path of your entry point
+  */
   entry: {
     app: path.join(__dirname, 'src/index.js')
   },
+  /*
+    Output,
+    path= place to put your final files
+    filename= name of your app file
+  */
   output: {
     path: path.join(__dirname, '/dist/'),
-    filename: '[name].min.js',
-    publicPath: '/'
+    filename: 'app.min.js'
   },
+  /*
+    Plugins, webpack has hundred of plugins to automate some tasks
+    Check this out https://webpack.js.org/plugins/
+  */
   plugins: [
+    // Remove dist folder before create a new one
     new CleanWebpackPlugin(['dist']),
+    // Easily create HTML files to serve your bundles, to configurate https://github.com/jantimon/html-webpack-plugin#options
     new HtmlWebpackPlugin({
       title: 'Coso'
     })
   ],
+
   module: {
+    /*
+      Rules declare witch loader it should use when a file match with test value,
+      test= regex to match
+      exclude= path where webpack will not watch
+      loader= what webpack should use with that file
+      query= extra configuration
+    */
     rules: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
@@ -39,7 +63,7 @@ const config = {
 
 module.exports = (env, argv) => {
   if (argv.mode === 'development') {
-    config.devtool = 'eval-source-map';
+    // if dev mode add a http server with desired content, more configuration https://webpack.js.org/configuration/dev-server/
     config.devServer = {
       contentBase: './dist'
     }
